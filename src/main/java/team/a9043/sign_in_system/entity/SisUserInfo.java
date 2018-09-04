cfee3e7b71e8d67d8abb7a052d36d957853ee920
a9043 <zzz13129180808@gmail.com>
2018-09-04 13:30:56 +0800

    sisUserInfo

---
+++ src/main/java/team/a9043/sign_in_system/entity/SisUserInfo.java
@@ -0,0 +1,27 @@
+package team.a9043.sign_in_system.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@DynamicUpdate
+@DynamicInsert
+public class SisUserInfo implements Serializable {
+    @Id
+    @NotNull
+    @OneToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "suId", referencedColumnName = "suId")
+    private SisUser suId;
+
+    @Column(length = 20)
+    @NotNull
+    private Integer lackNum = 0;
+}
--- src/main/java/team/a9043/sign_in_system/schedule/AttRateSchedule.java
+++ src/main/java/team/a9043/sign_in_system/schedule/AttRateSchedule.java
@@ -3,16 +3,88 @@ package team.a9043.sign_in_system.schedule;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import team.a9043.sign_in_system.mapper.SisCourseMapper;
+import team.a9043.sign_in_system.mapper.SisScheduleMapper;
+import team.a9043.sign_in_system.mapper.SisSupervisionMapper;
+import team.a9043.sign_in_system.pojo.*;
+import team.a9043.sign_in_system.util.judgetime.InvalidTimeParameterException;
+import team.a9043.sign_in_system.util.judgetime.JudgeTimeUtil;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class AttRateSchedule {
     @Resource
     private SisCourseMapper sisCourseMapper;
+    @Resource
+    private SisSupervisionMapper sisSupervisionMapper;
+    @Resource
+    private SisScheduleMapper sisScheduleMapper;
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void attRateSchedule() {
         //
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void supervisionSchedule() {
+        try {
+            int week = JudgeTimeUtil.getWeek(LocalDate.now()) - 1;
+
+            //获得督导课程
+            SisCourseExample sisCourseExample = new SisCourseExample();
+            sisCourseExample.createCriteria().andScNeedMonitorEqualTo(true).andSuIdIsNotNull();
+            List<SisCourse> sisCourseList = sisCourseMapper.selectByExample(sisCourseExample);
+
+            //获得课程排课
+            List<String> scIdList = sisCourseList.parallelStream()
+                .map(SisCourse::getScId)
+                .distinct()
+                .collect(Collectors.toList());
+            if (scIdList.isEmpty())
+                return;
+            SisScheduleExample sisScheduleExample = new SisScheduleExample();
+            sisScheduleExample.createCriteria().andScIdIn(scIdList);
+            List<SisSchedule> sisScheduleList = sisScheduleMapper.selectByExample(sisScheduleExample);
+
+            //获得督导历史
+            List<Integer> ssIdList = sisScheduleList.parallelStream().map(SisSchedule::getSsId).distinct().collect(Collectors.toList());
+            if (ssIdList.isEmpty())
+                return;
+
+            SisSupervisionExample sisSupervisionExample = new SisSupervisionExample();
+            sisSupervisionExample.createCriteria().andSsIdIn(ssIdList).andSsvWeekLessThanOrEqualTo(week);
+            List<SisSupervision> sisSupervisionList = sisSupervisionMapper.selectByExample(sisSupervisionExample);
+
+            sisCourseList.parallelStream().map(sisCourse -> {
+                String scId = sisCourse.getScId();
+
+                List<SisSchedule> scheduleList = sisScheduleList.parallelStream()
+                    .filter(sisSchedule -> sisSchedule.getScId().equals(scId))
+                    .collect(Collectors.toList());
+
+                if (scheduleList.isEmpty())
+                    return null;
+                int toalLackNum = scheduleList.parallelStream()
+                    .map(sisSchedule -> {
+                        Integer ssId = sisSchedule.getSsId();
+                        int countNum = (int) sisSupervisionList.parallelStream()
+                            .filter(sisSupervision -> sisSupervision.getSsId().equals(ssId))
+                            .count();
+
+                        int lackNum = week - countNum;
+                        lackNum = lackNum < 0 ? 0 : lackNum;
+                        return lackNum;
+                    })
+                    .reduce(0, Integer::sum);
+                return null;
+            });
+        } catch (InvalidTimeParameterException e) {
+            e.printStackTrace();
+        }
     }
 }
