a316a47a9d42bfda74d1cd87df3b40b1db9d4a63
a9043 <zzz13129180808@gmail.com>
2018-09-14 18:38:03 +0800

    async mail sender

--- src/main/java/team/a9043/sign_in_system/service/ContactService.java
+++ src/main/java/team/a9043/sign_in_system/service/ContactService.java
@@ -1,28 +1,23 @@
 package team.a9043.sign_in_system.service;
 
-import org.json.JSONObject;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import team.a9043.sign_in_system.async.AsyncSendMail;
 import team.a9043.sign_in_system.mapper.SisContactMapper;
 import team.a9043.sign_in_system.pojo.SisContact;
 
 import javax.annotation.Resource;
 
 @Service
+@Slf4j
 public class ContactService {
     @Resource
     private SisContactMapper sisContactMapper;
     @Resource
-    private MailSender mailSender;
+    private AsyncSendMail asyncSendMail;
 
     public boolean receiveKssContact(SisContact sisContact) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("2541175183@qq.com");
-        mailMessage.setSubject(sisContact.getSctName());
-        mailMessage.setText(new JSONObject(sisContact).toString(2));
-        mailMessage.setTo("a90434957@live.cn");
-        mailSender.send(mailMessage);
+        asyncSendMail.sendMail(sisContact);
         return sisContactMapper.insert(sisContact) > 0;
     }
 }
