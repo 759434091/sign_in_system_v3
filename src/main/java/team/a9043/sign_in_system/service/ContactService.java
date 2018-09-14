a3c01a507d3723ae629580587814aa66d26dac4b
a9043 <zzz13129180808@gmail.com>
2018-09-14 11:41:20 +0800

    init contact

---
+++ src/main/java/team/a9043/sign_in_system/service/ContactService.java
@@ -0,0 +1,10 @@
+package team.a9043.sign_in_system.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+    public boolean receiveKssContact() {
+        return false;
+    }
+}
---
+++ src/main/java/team/a9043/sign_in_system/service_pojo/KssContact.java
@@ -0,0 +1,12 @@
+package team.a9043.sign_in_system.service_pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KssContact {
+    private String cName;
+    private String cContact;
+    private String cContent;
+}
