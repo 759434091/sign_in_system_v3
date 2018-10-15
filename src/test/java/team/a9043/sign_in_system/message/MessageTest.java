cc9232b880a3c651cc51e97cc5b4f18765e971cc
a9043 <zzz13129180808@gmail.com>
2018-10-15 13:04:30 +0800

    opt

--- src/test/java/team/a9043/sign_in_system/message/MessageTest.java
+++ src/test/java/team/a9043/sign_in_system/message/MessageTest.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import team.a9043.sign_in_system.mapper.SisUserMapper;
-import team.a9043.sign_in_system.pojo.SisSchedule;
-import team.a9043.sign_in_system.pojo.SisUser;
 import team.a9043.sign_in_system.service.MessageService;
 
 import javax.annotation.Resource;
